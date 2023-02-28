bc6d2836ced1c2900860e2c76323eb35c20eaf8e
Ravish Shardha <ravishshardha@tamu.edu>
2023-02-27 21:27:53 -0600

    added a query: selects and prints the employee id and name of servers and cashiers who are clocked in

--- EmployeeData.sql
+++ EmployeeData.sql
@@ -1,1 +1,8 @@
-SELECT * FROM employeedata;
+-- Selects all entries from table
+SELECT * FROM employeedata; 
+
+
+-- Prints all the avaliable servers and cashier
+SELECT emp_id, emp_name
+FROM employeedata
+WHERE position IN ('Server', 'Cashier') AND clock_in_out_status = 'In';
