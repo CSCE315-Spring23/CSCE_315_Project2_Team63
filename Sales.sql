6dd4be1da4a7b6854753853f29164fe3afd7a85b
connorh3333 <crh2000@tamu.edu>
2023-02-28 17:36:14 -0600

    sales table sql push

---
+++ Sales.sql
@@ -0,0 +1,15 @@
+-- Prints every entry from table
+
+SELECT * FROM sales;
+
+
+-- Gets largest sales amount
+
+SELECT MAX(total sales)
+FROM sales;
+
+
+-- Gets the total sum of the sales
+
+SELECT SUM(total sales)
+FROM sales;
