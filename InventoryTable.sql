04a9b37e1d0991231c7dbd28ef3a2fa539d8c8c6
garrettguerrero <gguerrero@tamu.edu>
2023-02-28 21:46:46 -0600

    added function that shows the total quantity of items from each vendor in inventory_table.

--- InventoryTable.sql
+++ InventoryTable.sql
@@ -1,2 +1,7 @@
 -- displays all items in inventory_table
 SELECT * FROM inventory_table;
+
+-- shows the total quantity of items from each vendor
+SELECT vendor_name, SUM(quantity) AS total_quantity
+FROM inventory_table
+GROUP BY vendor_name;
