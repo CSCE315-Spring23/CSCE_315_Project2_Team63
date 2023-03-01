5b25122c5fbd92df2ee33eaf523c9fc6ca20e41c
Daniel <djs2002@tamu.edu>
2023-02-28 21:35:52 -0600

    Added a query that finds the maximum order in the total column

--- OrderHistory.sql
+++ OrderHistory.sql
@@ -3,4 +3,6 @@ SELECT * FROM orderhistory;
 -- Show sum up to 1 million
 SELECT SUM(total) AS total_total FROM orderhistory;
 
--- 
+-- Find the maximum total in the total column
+SELECT MAX(total) FROM orderhistory;
+
