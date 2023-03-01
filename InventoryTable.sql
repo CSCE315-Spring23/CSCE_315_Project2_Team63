0eee387524377b952c1891b9b2fd5694eb0e02a2
garrettguerrero <gguerrero@tamu.edu>
2023-02-28 21:49:24 -0600

    added function that displays all items whose prices are greater than the average price of all items

--- InventoryTable.sql
+++ InventoryTable.sql
@@ -5,3 +5,8 @@ SELECT * FROM inventory_table;
 SELECT vendor_name, SUM(quantity) AS total_quantity
 FROM inventory_table
 GROUP BY vendor_name;
+
+-- shows all items purchased for greater than the average price of all items
+SELECT *
+FROM inventory_table
+WHERE price > (SELECT AVG(price) FROM inventory_table);
