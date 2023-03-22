0da06c69d94e5def8ccbc031ba4a517ee9c96347
Daniel <djs2002@tamu.edu>
2023-03-21 19:27:39 -0500

    added to main the implementation of the homepage

--- JavaGUI/Main.java
+++ JavaGUI/Main.java
@@ -1,6 +1,11 @@
-
+/**
+The Main class is the entry point of the application.
+*/
 public class Main{
-
+    /**
+    The main method creates an instance of HomePage and displays it.
+    @param args The command-line arguments passed to the application
+    */
     public static void main(String[] args)
     {
         HomePage homePage = new HomePage();
